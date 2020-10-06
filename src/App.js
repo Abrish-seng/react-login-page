3ec3affb5555281fe4af53a72cf20574667d3c1d
abroa <abrish814@gmail.com>
2020-10-06 16:43:05 +0300

    text field,button and label added to form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import { AppBar } from 'material-ui';
-import React from 'react';
+
+import React, { Component } from 'react';
 import './App.css';
-function App() {
-  return (
+class App extends Component {
+ state={
+   firstName:'',
+   lastName:'',
+   email:'',
+   password:'',
+   signUp:''
+ }
+ handleChange=input=>e=>{
+   this.setState({[input]:e.target.values})
+ }
+render(){
+const {firstName,lastName,email,password,signUp}=this.state
+const values={firstName,lastName,email,password,signUp}  
+return (
     <div className="wrapper">
-      <h2>Create Account</h2>
-      <form onSubmit={this.handleSubmit} noValidate>
+     <div className="form-wrapper">
+     <h2>Create Account</h2>
+      <form >
        <div className="firstName">
        <label htmlFor="firstName">First Name</label>
-       <input type="text"  className="" placeholder="First Name" name="firstName"/>
+       <input type="text"  className="form-control" placeholder="First Name" name="firstName" onChange={this.handleChange}
+       />
+       </div>
+       <div className="lastName">
+       <label htmlFor="lastName">Last Name</label>
+       <input type="text"  className="form-control" placeholder="Last Name" name="lastName" onChange={this.handleChange}/>
+       </div>
+       <div className="email">
+       <label htmlFor="email">Email</label>
+       <input type="email"  className="form-control" placeholder="Email" name="email" onChange={this.handleChange}/>
+       </div>
+       <div className="password">
+       <label htmlFor="password">Password</label>
+       <input type="password"  className="form-control" placeholder="password" name="password" onChange={this.handleChange}/>
+       </div>
+       <div className="createAccount">
+       <button type="submit" >Create Account </button>
+       <small>Already Have an Account?</small>
        </div>
       </form>
+     </div>
     </div>
   );
+}
 }
 
 export default App;
